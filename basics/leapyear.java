b6f6241997e57d62e78c2f2381cf43753844a85a
Snehal Pandey <snehalpandey183nit@gmail.com>
2025-08-22 21:45:04 +0530

    Add files via upload

---
+++ basics/leapyear.java
@@ -0,0 +1,16 @@
+package conditionals;
+import java.util.Scanner;
+
+public class leapyear {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter the year");
+        int n= sc.nextInt();
+        if(n%4==0 && n%100 !=0|| n%400==0){
+            System.out.println("leap year");
+        }
+        else{
+            System.out.println("not a leap year");
+        }
+    }
+}
